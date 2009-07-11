dbae438ae7f7e26b42233d6666273767ef823004
Thomas Kern <thomaskern@noname.(none)>
2009-07-11 11:49:10 +0200

    license added to new files

--- test/data/parameterizable_pseudo_class.rb
+++ test/data/parameterizable_pseudo_class.rb
@@ -1,6 +1,13 @@
+# Author::    Thomas Kern
+# License::   MPL 1.1
+# Project::   ai4r
+# Url::       http://ai4r.rubyforge.org/
+#
+# You can redistribute it and/or modify it under the terms of
+# the Mozilla Public License version 1.1  as published by the
+# Mozilla Foundation at http://www.mozilla.org/MPL/MPL-1.1.txt
+
 require File.dirname(__FILE__) + '/../../lib/ai4r/data/parameterizable'
-
-
 
 class ParameterizablePseudoClass
   include Ai4r::Data::Parameterizable
